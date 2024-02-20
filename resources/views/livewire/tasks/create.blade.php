4e655b9b4a607e6fe86543651b76db228ea4cd1d
Saullo Almeida <saullorhamon@gmail.com>
2024-02-19 23:10:59 -0300

    create Task model and relationship
    also created new task funcionality

--- resources/views/livewire/tasks/create.blade.php
+++ resources/views/livewire/tasks/create.blade.php
@@ -5,24 +5,24 @@ use Illuminate\Support\Facades\Auth;
 use function Livewire\Volt\{state, rules};
 
 state([
-    'task' => '',
+    'title' => '',
 ]);
 
 rules([
-    'task' => 'required|max:255',
+    'title' => 'required|max:255',
 ]);
 
 $store = function () {
     $validated = $this->validate();
-    Auth::user()->tasks()->create($validated);
-    $this->task = '';
+    auth()->user()->tasks()->create($validated);
+    $this->title = '';
 };
 
 ?>
 
 <div class="flex flex-col gap-2">
     <form wire:submit="store">
-        <x-ts-input label="Tarefa" wire:model="task"/>
+        <x-ts-input label="Tarefa" wire:model="title"/>
         <x-ts-button square>TallStackUi</x-ts-button>
     </form>
 
